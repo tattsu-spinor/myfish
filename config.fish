ee93106ca252003023904e288cae2fbbe9fb76f1
たっつー <98200460+tattsu-spinor@users.noreply.github.com>
2023-04-21 08:29:47 +0900

    プロンプトをstarshipで管理するよう変更

--- config.fish
+++ config.fish
@@ -1,21 +1,8 @@
 ### Theme (Dracula)
 fish_config theme choose "Dracula Official"
 
-### Prompt (bobthefish)
-set -g theme_display_git_default_branch yes
-set -g theme_display_docker_machine yes
-set -g theme_title_display_process yes
-# set -g theme_display_user yes
-# set -g theme_display_hostname yes
-# set -g theme_display_sudo_user yes
-set -g theme_date_format "+%Y/%m/%d %a %H:%M:%S"
-set -g theme_nerd_fonts yes
-set -g theme_show_exit_status yes
-set -g theme_display_jobs_verbose yes
-set -g theme_color_scheme nord
-set -g fish_prompt_pwd_dir_length 0
-set -g theme_newline_cursor yes
-set -g theme_newline_prompt '$ '
+### Prompt (Starship)
+starship init fish | source
 
 ### Key Bindings
 function fish_user_key_bindings
