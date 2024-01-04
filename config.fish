fa97f4e30eb30ff640d1003d606fb1ed2a24a024
たっつー <98200460+tattsu-spinor@users.noreply.github.com>
2024-01-04 10:26:20 +0900

    環境変数を定義

--- config.fish
+++ config.fish
@@ -1,3 +1,17 @@
+### Env
+# bat
+set -gx BAT_STYLE grid
+# brew
+set -gx HOMEBREW_BUNDLE_FILE_GLOBAL $HOME/.config/brew/Brewfile
+# editor
+set -gx EDITOR nvim
+# java
+set -gx JAVA_HOME (/usr/libexec/java_home)
+# lazygit
+set -gx XDG_CONFIG_HOME $HOME/.config
+# locale
+set -gx LC_ALL $LANG
+
 ### Theme (Dracula)
 fish_config theme choose "Dracula Official"
 
